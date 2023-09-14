9e08089b223d14dc7b7709c38bb5270fb8c254a7
Okechukwu Chidera <okechukwuchidera12@gmail.com>
2023-09-14 18:14:17 +0100

    backdrop

--- src/Main.js
+++ src/Main.js
@@ -1,40 +1,38 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios';
-import requests from './Request'
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import requests from "./Request";
 
 const Main = () => {
+  const [movies, setmovies] = useState([]);
 
-    const [movies, setmovies] = useState([])
+  const movie = movies[Math.floor(Math.random() * movies.length)];
 
-    const movie = movies[Math.floor(Math.random()* movies.length)]
+  useEffect(() => {
+    axios
+      .get(requests.requestTrending)
+      .then((response) => {
+        setmovies(response.data.results);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
 
-    useEffect (() =>{
-        axios.get(requests.requestTrending).then((response)=>{
-            setmovies(response.data.results)
-        })
-        .catch((error) => {
-            console.error('Error fetching data:', error);
-        });
-    },[])
-
-    console.log ('Movie Data:', movies)
-
+  console.log("Movie Data:", movies);
 
   return (
-    <div className='w-full h-{550px} text-white'>
-
-       {movies.length > 0 && (
-
-
-         <div className='w-full h-full'>
-            <img src={`https://api.themoviedb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
-         </div>
-
-        )}
+    <div className="w-full h-{550px} text-white">
+      {movies.length > 0 &&
+        movies.map((mov, i) => (
+          <div key={i}>
+            <h3>{mov.title}</h3>
+            <img
+              src={`https://image.tmdb.org/t/p/original${mov.backdrop_path}`}
+            />
+          </div>
+        ))}
     </div>
-  )
-
-}
+  );
+};
 
 export default Main;
